bca1be0e0583ec1a9921a71577b1dfca5e93b523
Snake O Mania <chinmaynangia52@gmail.com>
2021-03-18 16:40:40 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from turtle import Screen
+from Snake import Snake
+from Food import Food
+from scoreboard import ScoreBoard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake O Mania")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+is_game_on = True
+
+while is_game_on:
+    screen.update()
+    time.sleep(.1)
+    snake.move()
+    # detecting the collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        is_game_on = False
+    for segments in snake.segments[1:]:
+        if snake.head.distance(segments) < 10:
+            is_game_on = False
+            scoreboard.game_over()
+
+
+
+screen.exitonclick()
